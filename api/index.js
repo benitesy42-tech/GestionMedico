23578de9b433970ba7bc7293530c22497f7fc845
Yordi Benites <benitesy42@gmail.com>
2026-05-30 01:59:37 -0500

    fix: remove packageManager, add .vercelignore, robust api handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,12 @@
 const app = require('../backend/src/app');
 
-module.exports = app;
+module.exports = async (req, res) => {
+  try {
+    await app(req, res);
+  } catch (err) {
+    console.error('Vercel API Error:', err);
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ error: 'Internal Server Error' }));
+  }
+};
